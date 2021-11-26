b5f829ccf95d55d88d83a1b7251dbfcf581821b1
johaolopz <johao.lopz.m@gmail.com>
2021-11-25 19:16:18 -0500

    using Material UI, @mui/icons-material

--- src/components/TodoDetail/TodoDetail.jsx
+++ src/components/TodoDetail/TodoDetail.jsx
@@ -3,6 +3,9 @@ import { useParams, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux";
 import { toInProgress, toDone, removeTodo, details } from '../../redux/actions/index';
 import "./todoDetail.css"
+//MATERIAL UI
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export function TodoDetail() {
     const dispatch = useDispatch();
@@ -21,9 +24,9 @@ export function TodoDetail() {
               <div>Date: {detailTodo[0].date}</div>
           </div>
           <div className="buttons">
-              <button onClick={() => dispatch(toDone(parseInt(params.id)))} >DONE</button>
-              <button onClick={() => dispatch(toInProgress(parseInt(params.id)))} >IN PROGRESS</button>
-              <button onClick={() => dispatch(removeTodo(parseInt(params.id)))} >DELETE</button>
+              <Button variant="contained" color="success" id="doneButton" onClick={() => dispatch(toDone(parseInt(params.id)))} >DONE</Button>
+              <Button variant="contained" color="secondary" id="inProgressButton" onClick={() => dispatch(toInProgress(parseInt(params.id)))} >IN PROGRESS</Button>
+              <Button variant="outlined" color="error" startIcon={<DeleteIcon />} id="deleteButton" onClick={() => dispatch(removeTodo(parseInt(params.id)))} >DELETE</Button>
           </div>
           <div>
             <Link exact to="/" className="link">Home</Link>
